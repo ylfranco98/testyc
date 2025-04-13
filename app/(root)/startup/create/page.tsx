19f1fb05b1890e0079222ea0cb733a7ad2a3b195
Yoana <ylfranco98@gmail.com>
2025-04-13 09:00:34 -0400

    first commit

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -1,21 +1,21 @@
-import StartupForm from "@/components/StartupForm";
-import { auth } from "@/auth";
-import { redirect } from "next/navigation";
+// import StartupForm from "@/components/StartupForm";
+// import { auth } from "@/auth";
+// import { redirect } from "next/navigation";
 
-const Page = async () => {
-  const session = await auth();
+// const Page = async () => {
+//   const session = await auth();
 
-  if (!session) redirect("/");
+//   if (!session) redirect("/");
 
-  return (
-    <>
-      <section className="pink_container !min-h-[230px]">
-        <h1 className="heading">Submit Your Startup</h1>
-      </section>
+//   return (
+//     <>
+//       <section className="pink_container !min-h-[230px]">
+//         <h1 className="heading">Submit Your Startup</h1>
+//       </section>
 
-      <StartupForm />
-    </>
-  );
-};
+//       <StartupForm />
+//     </>
+//   );
+// };
 
-export default Page;
+// export default Page;
--- components/View.tsx
+++ components/View.tsx
@@ -2,7 +2,7 @@ import Ping from "@/components/Ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 import { writeClient } from "@/sanity/lib/write-client";
-import { unstable_after as after } from "next/server";
+import { after as after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
@@ -14,7 +14,7 @@ const View = async ({ id }: { id: string }) => {
       await writeClient
         .patch(id)
         .set({ views: totalViews + 1 })
-        .commit(),
+        .commit()
   );
 
   return (
